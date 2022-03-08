016adc1850fe27b1b629b6df991f92b744fadc26
Airrocket <airrocket.buksas@gmail.com>
2022-03-08 00:05:02 -0700

    Initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,18 @@
+<style lang="scss">
+div {
+    background-color: $bg-secondary;
+    color: $text;
+    display: flex;
+    justify-content: center;
+    * {
+        padding: 1rem;
+    }
+}
+
+</style>
+
+<div>
+    <h1>This is an h1</h1>
+</div>
+
+<slot></slot>

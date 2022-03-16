a47f7a513fac84aba9f28fbf30df29af39ea2847
Airrocket <airrocket.buksas@gmail.com>
2022-03-16 02:57:45 -0600

    spite

---
+++ src/routes/extra/__layout.reset.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+    <title>&#060;Kayt.dev/&#062;</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="manifest" href="/site.webmanifest" />
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+    <meta name="description" content="trans things at data" />
+    <meta name="author" content="kayt_was_taken" />
+</svelte:head>
+
+<slot/>
---
+++ src/routes/extra/data-trans-things.svelte
@@ -0,0 +1,32 @@
+<div class="container">
+    <div>
+        <h1>Who runs the account?</h1>
+        <p>Me. Kaytlyn Buksas, a junior at Data.</p>
+        <h1>Don’t you know you’re not supposed to use the school name and logo?</h1>
+        <p>Yeah. I don’t care.</p>
+        <h1>Why don’t you just get permission to the use the school’s IP?</h1>
+        <p>It sounds boring and I don’t like bureaucracy.</p>
+        <h1>Why did you make the account?</h1>
+        <p>I’m generally frustrated with how hard being trans and finding people like me is and I wanted to make that more visible. I also think the way admin reacted to the other accounts was overblown and this is how I want to show that.</p>
+        <h1>What if you get in trouble for making the account?</h1>
+        <p>I’ll accept it, though I don’t think punishing a trans girl for expressing herself is a good look. If I’m told to shut down the account I’ll just make a new one.</p>
+        <h1>What’s the point of the account?</h1>
+        <p>Any trans related things I guess. Creative outlets, coming out, resources, support, opinions, questions, etc.</p>
+        <h1>What if no one cares?</h1>
+        <p>Oh well, I tried. And to me that’s the most important.</p>
+    </div>
+</div>
+
+<style lang="scss">
+    .container {
+        display: flex;
+        height: 100vh;
+        justify-content: center;
+        
+        div {
+            width: 50vw;
+
+            @include descending-width
+        }
+    }
+</style>
